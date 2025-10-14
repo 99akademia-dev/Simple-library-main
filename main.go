99878389b8c7ae551343c9c55c7ccd0d426345ef
Irbek <irbek106@gmail.com>
2025-10-14 02:56:36 -0700

    изменененияй

--- main.go
+++ main.go
@@ -3,26 +3,38 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Проект 'Простая библиотека' запущен!")
-    
-    book := Book{
-        ID:       1,
-        Title:    "Война и мир",
-        Author:   "Лев Толстой",
-        IsIssued: false,
-    }
-    
-    reader := Reader{
-        ID:   1,
-        Name: "Иван Иванов",
-    }
-    
-    fmt.Printf("Книга: %s, Автор: %s\n", book.Title, book.Author)
-    fmt.Printf("Читатель: %s\n", reader.Name)
-    
-    if book.IsIssued {
-        fmt.Println("Книга выдана")
-    } else {
-        fmt.Println("Книга доступна")
-    }
+	user1 := Reader{
+		ID:        1,
+		FirstName: "Агунда",
+		LastName:  "Абаева",
+		IsActive:  true,
+	}
+
+	book1 := Book{
+		ID:       1,
+		Title:    "Преступление и наказание",
+		Author:   "Федор Достоевский",
+		Year:     1866,
+		IsIssued: false,
+	}
+
+	fmt.Println("=== Демонстрация работы библиотеки ===")
+	fmt.Printf("Книга: %s\n", book1.String())
+	fmt.Printf("Статус выдачи: %t\n", book1.IsIssued)
+	fmt.Println("\n--- Выдача книги активному читателю ---")
+	book1.IssueBook(&user1)
+	fmt.Printf("Статус выдачи после попытки выдачи: %t\n", book1.IsIssued)
+	fmt.Println("\n--- Повторная выдача книги ---")
+	book1.IssueBook(&user1)
+	fmt.Println("\n--- Возврат книги ---")
+	book1.ReturnBook()
+	fmt.Printf("Статус выдачи после возврата: %t\n", book1.IsIssued)
+	fmt.Println("\n--- Выдача книги неактивному читателю ---")
+	user1.Deactivate()
+	book1.IssueBook(&user1)
+	fmt.Println("\n--- Выдача книги после активации ---")
+	user1.Activate()
+	book1.IssueBook(&user1)
+	fmt.Println("\n--- Информация о читателе ---")
+	fmt.Println(user1.String())
 }
--- models.go
+++ models.go
@@ -1,13 +1,68 @@
 package main
 
-type Book struct {
-    ID       int
-    Title    string
-    Author   string
-    IsIssued bool
-}
+import "fmt"
 
 type Reader struct {
-    ID   int
-    Name string
+	ID        int
+	FirstName string
+	LastName  string
+	IsActive  bool
+}
+
+func (r Reader) DisplayReader() {
+	fmt.Printf("читатель: %s %s (ID: %d)\n", r.FirstName, r.LastName, r.ID)
+}
+
+func (r *Reader) Deactivate() {
+	r.IsActive = false
+	fmt.Printf("читатель %s %s деактивирован\n", r.FirstName, r.LastName)
+}
+
+func (r *Reader) Activate() {
+	r.IsActive = true
+	fmt.Printf("читатель %s %s активаирован\n", r.FirstName, r.LastName)
+}
+
+func (r Reader) String() string {
+	status := ""
+	if r.IsActive {
+		status = "Активен"
+	} else {
+		status = "Неактивен"
+	}
+	return fmt.Sprintf("Пользователь %s %s, ID: %d, статус: %s", r.FirstName, r.LastName, r.ID, status)
+}
+
+type Book struct {
+	ID       int
+	Title    string
+	Author   string
+	Year     int
+	IsIssued bool
+}
+
+func (b Book) String() string {
+	return fmt.Sprintf("\"%s\" (%s, %d)", b.Title, b.Author, b.Year)
+}
+
+func (b *Book) IssueBook(reader *Reader) {
+	if b.IsIssued {
+		fmt.Printf("Книга %s уже выдана\n", b.Title)
+		return
+	}
+	if !reader.IsActive {
+		fmt.Printf("читатель %s %s не активен и не может получить книгу\n", reader.FirstName, reader.LastName)
+		return
+	}
+	b.IsIssued = true
+	fmt.Printf("книга %s выдана читателю %s %s\n", b.Title, reader.FirstName, reader.LastName)
+}
+
+func (b *Book) ReturnBook() {
+	if !b.IsIssued {
+		fmt.Printf("книга %s и так в библиотеке\n", b.Title)
+		return
+	}
+	b.IsIssued = false
+	fmt.Printf("книга %s возвращена в библиотеку\n", b.Title)
 }
